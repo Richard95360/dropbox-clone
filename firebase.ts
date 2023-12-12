f3c35612615cea1fd64d209d4cb314f573765233
Votre Nom <Vous@exemple.com>
2023-12-12 13:37:33 +0100

    for  commit

--- firebase.ts
+++ firebase.ts
@@ -3,12 +3,13 @@ import { getFirestore } from "firebase/firestore";
 import { getStorage } from "firebase/storage";
 
 const firebaseConfig = {
-  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
-  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
-  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
-  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+apiKey: "AIzaSyCMbHAT2MnIDOsAbeBvViXDxXi_mCDN05Y",
+authDomain: "dropbox-clone-7a286.firebaseapp.com",
+projectId: "dropbox-clone-7a286",
+storageBucket: "dropbox-clone-7a286.appspot.com",
+messagingSenderId: "540886569714",
+appId: "1:540886569714:web:5335710e02987b7677366b",
+measurementId: "G-JN824V510B"
 };
 
   const app = getApps().length ? getApp() : initializeApp(firebaseConfig);
